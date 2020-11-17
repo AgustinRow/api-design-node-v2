15ff9265fb10ebcf041316d22da31b2c97279914
AgustinRow <agus.diaz.11@gmail.com>
2020-11-17 09:33:40 -0300

    first exercise done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import http from 'http'
-import { createServer } from 'http'
+import http from "http";
+import { createServer } from "http";
 
-import app from './server'
-let currentApp = app
+import app from "./server";
+let currentApp = app;
 
-
+currentApp.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(["./server"], () => {
+    server.removeListener("request", currentApp);
+    server.on("request", app);
+    currentApp = app;
+  });
 }
